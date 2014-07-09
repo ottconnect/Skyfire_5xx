cf05fd03270fae95b24d47659d1e37de47c99ca8
gegge6265 <luigibalotta@live.it>
2014-07-09 15:31:39 +0200

    fixed derp

--- Pull_request_#56_fix_07_09_2014_327pm.sql
+++ Pull_request_#56_fix_07_09_2014_327pm.sql
@@ -1,7 +1,7 @@
 delete from npc_vendor where item not in (79249, 63388, 64670) and entry = 3323;
 replace into npc_vendor(entry, item) values(3323, 31167);
 delete from npc_vendor where item in(25843, 25861, 28979, 29007, 29008) and entry = 3314;
-delete from npc_vendor where item in(29446, 29469, 29470, 29472, 34129) and entry = 12796 and ExtendedCost in (423, 2570);
+delete from npc_vendor where item in(29466, 29469, 29470, 29472, 34129) and entry = 12796 and ExtendedCost in (423, 2570);
 update creature_template set gossip_menu_id = 0, npcflag = 128, AIName = '' where entry = 5188;
 update creature set position_z = 28.587 where guid = 1395815;
 update creature set position_z = 30.254 where guid = 1395820;
